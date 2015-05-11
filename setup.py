1c707d29204426af741fb51d7d2a5074bbfd2058
paul.rutledge <paul.rutledge@vodori.com>
2015-05-11 18:05:15 -0500

    updating pypi version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
 	name="django-commands",
-	version="0.6",
+	version="0.7",
 	author="Paul Rutledge",
 	author_email="paul.v.rutledge@gmail.com",
 	description="A django app that provides a plugin type model for integrating the front and backend.",
